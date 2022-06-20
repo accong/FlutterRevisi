7220afa49897044a6ddae43ee84f4484ee952eba
unknown <mbilalrafif@gmail.com>
2022-06-20 12:40:14 +0700

    update login_page dan register_page

--- lib/ui/login/login_view.dart
+++ lib/ui/login/login_view.dart
@@ -6,13 +6,12 @@ import 'package:tripkuy/ui/dashboard/view/dashboard.dart';
 import 'package:tripkuy/ui/register/register_view.dart';
 
 class LoginView extends StatelessWidget {
-
-  LoginView({ Key? key }) : super(key: key);
+  LoginView({Key? key}) : super(key: key);
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  onLogin(){
+  onLogin() {
     // if(emailController.text.isEmpty || passwordController.text.isEmpty){
     //   CoreFunction.showToast("Form tidak boleh kosong");
     // } else if (emailController.text != Constant.adminEmail) {
@@ -20,7 +19,7 @@ class LoginView extends StatelessWidget {
     // } else if (passwordController.text != Constant.adminPassword) {
     //   CoreFunction.showToast("Password Salah");
     // } else if (emailController.text == Constant.adminEmail &&passwordController.text == Constant.adminPassword){
-      pushAndRemoveUntil(Dashboard(), RouterType.material);
+    pushAndRemoveUntil(Dashboard(), RouterType.material);
     // } else {
     //   CoreFunction.showToast("Uknow Action");
     // }
@@ -50,48 +49,60 @@ class LoginView extends StatelessWidget {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Component.textBold(
-                    "Welcome Back!",
-                    fontSize: 16,
-                    colors: ColorPalette.black
+                  Component.textBold("Welcome Back!",
+                      fontSize: 16, colors: ColorPalette.black),
+                  const SizedBox(
+                    height: 20,
                   ),
-                  const SizedBox(height: 20,),
                   Image.asset(
                     "assets/images/image_login.png",
                     width: SizeConfig.blockSizeHorizontal * 50,
                     height: SizeConfig.blockSizeHorizontal * 50,
                   ),
-                  const SizedBox(height: 20,),
-                  TextField(
-                    keyboardType: TextInputType.text,
-                    textInputAction: TextInputAction.next,
-                    controller: emailController,
-                    style: const TextStyle(fontSize: 14, color: ColorPalette.grey),
-                    decoration: Component.decorationNoBorder("Email", iconPrefix: Icons.email)
+                  const SizedBox(
+                    height: 20,
                   ),
-                  const SizedBox(height: 20,),
                   TextField(
-                    keyboardType: TextInputType.text,
-                    textInputAction: TextInputAction.done,
-                    controller: passwordController,
-                    style: const TextStyle(fontSize: 14, color: ColorPalette.grey),
-                    decoration: Component.decorationNoBorder("Password", iconPrefix: Icons.lock)
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.next,
+                      controller: emailController,
+                      style: const TextStyle(
+                          fontSize: 14, color: ColorPalette.black),
+                      decoration: Component.decorationNoBorder("Email",
+                          iconPrefix: Icons.email)),
+                  const SizedBox(
+                    height: 20,
                   ),
-                  const SizedBox(height: 20,),
-                  Component.button(label: "Log in", onPressed: (){
-                    onLogin();
-                  }),
-                  const SizedBox(height: 15,),
+                  TextField(
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.done,
+                      controller: passwordController,
+                      style: const TextStyle(
+                          fontSize: 14, color: ColorPalette.black),
+                      decoration: Component.decorationNoBorder("Password",
+                          iconPrefix: Icons.lock)),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Component.button(
+                      label: "Log in",
+                      onPressed: () {
+                        onLogin();
+                      }),
+                  const SizedBox(
+                    height: 15,
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Component.textDefault("Don't have an account ? "),
                       InkWell(
-                        onTap: () {
-                          routePush(Register(), RouterType.material);
-                        },
-                        child: Component.textBold("Sign Up", colors: ColorPalette.blue)
-                      ),
+                          onTap: () {
+                            routePush(Register(), RouterType.material);
+                          },
+                          child: Component.textBold("Sign Up",
+                              colors: ColorPalette.blue)),
                     ],
                   )
                 ],
--- lib/ui/register/register_view.dart
+++ lib/ui/register/register_view.dart
@@ -5,22 +5,23 @@ import 'package:tripkuy/core/util/util.dart';
 import '../component.dart';
 
 class Register extends StatelessWidget {
-  
-  Register({ Key? key }) : super(key: key);
+  Register({Key? key}) : super(key: key);
 
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordConfirmController = TextEditingController();
+  final TextEditingController passwordConfirmController =
+      TextEditingController();
 
-  onRegister(){
-    if(formKey.currentState!.validate()){
-      CoreFunction.showToast("Berhasil Regis", backgroundColor: ColorPalette.white);
+  onRegister() {
+    if (formKey.currentState!.validate()) {
+      CoreFunction.showToast("Berhasil Regist",
+          backgroundColor: ColorPalette.white);
       // routePush(const HomeView(), RouterType.material);
     } else {
-      CoreFunction.showToast("Uknow Action");
+      CoreFunction.showToast("Uknown Action");
     }
   }
 
@@ -36,18 +37,18 @@ class Register extends StatelessWidget {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Component.textBold(
-                  "Register",
-                  fontSize: 17,
-                  colors: ColorPalette.black
+                Component.textBold("Register",
+                    fontSize: 17, colors: ColorPalette.black),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 8,
                 ),
-                SizedBox(height: SizeConfig.blockSizeVertical * 8,),
                 TextFormField(
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.next,
                   controller: usernameController,
-                  style: const TextStyle(fontSize: 14, color: Colors.white),
-                  decoration: Component.decorationNoBorder("Username", iconPrefix: Icons.email),
+                  style: const TextStyle(fontSize: 14, color: Colors.black),
+                  decoration: Component.decorationNoBorder("Username",
+                      iconPrefix: Icons.email),
                   validator: (value) {
                     if (value!.isEmpty) {
                       return 'masukkan username';
@@ -58,16 +59,19 @@ class Register extends StatelessWidget {
                     return null;
                   },
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.next,
                   controller: emailController,
-                  style: const TextStyle(fontSize: 14, color: Colors.white),
-                  decoration: Component.decorationNoBorder("Email", iconPrefix: Icons.email),
+                  style: const TextStyle(fontSize: 14, color: Colors.black),
+                  decoration: Component.decorationNoBorder("Email",
+                      iconPrefix: Icons.email),
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'masukkan Email';
+                      return 'masukkan email';
                     }
                     if (value.length < 8) {
                       return 'data tidak lengkap';
@@ -75,33 +79,40 @@ class Register extends StatelessWidget {
                     return null;
                   },
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
                   keyboardType: TextInputType.number,
                   textInputAction: TextInputAction.done,
                   controller: passwordController,
-                  style: const TextStyle(fontSize: 14, color: Colors.white),
-                  decoration: Component.decorationNoBorder("Phone Number", iconPrefix: Icons.lock),
+                  style: const TextStyle(fontSize: 14, color: Colors.black),
+                  decoration: Component.decorationNoBorder("Phone Number",
+                      iconPrefix: Icons.lock),
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'Enter password';
+                      return 'masukan phone number';
                     }
                     if (value.length < 8) {
-                      return 'please fill';
+                      return 'password kurang dari 8 karakter';
                     }
                     return null;
                   },
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
+                  obscureText: true,
                   keyboardType: TextInputType.text,
                   textInputAction: TextInputAction.done,
                   controller: passwordConfirmController,
-                  style: const TextStyle(fontSize: 14, color: Colors.white),
-                  decoration: Component.decorationNoBorder("Password", iconPrefix: Icons.lock),
+                  style: const TextStyle(fontSize: 14, color: Colors.black),
+                  decoration: Component.decorationNoBorder("Password",
+                      iconPrefix: Icons.lock),
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'Enter confirm password';
+                      return 'masukan password';
                     }
                     if (value.length < 8) {
                       return 'please fill';
@@ -109,21 +120,27 @@ class Register extends StatelessWidget {
                     return null;
                   },
                 ),
-                const SizedBox(height: 20,),
-                Component.button(label: "Register", onPressed: (){
-                  onRegister();
-                }),
-                const SizedBox(height: 15,),
+                const SizedBox(
+                  height: 20,
+                ),
+                Component.button(
+                    label: "Register",
+                    onPressed: () {
+                      onRegister();
+                    }),
+                const SizedBox(
+                  height: 15,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Component.textDefault("Already have an account ? "),
                     InkWell(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Component.textBold("Sign In", colors: ColorPalette.blue)
-                    ),
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Component.textBold("Sign In",
+                            colors: ColorPalette.blue)),
                   ],
                 )
               ],
